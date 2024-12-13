027441035ca58fc032bf849578bc021c0c562ef4
OsamabinAdnan <binadnanosama@gmail.com>
2024-12-13 22:06:51 +0500

    commit (hero updated)

--- assignments_details/react_component_custom_css/components/Hero/Hero.tsx
+++ assignments_details/react_component_custom_css/components/Hero/Hero.tsx
@@ -9,23 +9,23 @@ export default function Hero() {
   return (
     <>
         <section className={styles.bgImage}>
-            <div className='max-w-[1440px] mx-auto space-y-6  '>
-                <div className='top-24 relative space-y-6 max-w-2xl m-2'>
-                    <div className='space-y-8 '>
-                        <h1 className='text-white text-left md:text-[55px] text-[35px] font-bold'>Explore Pakistan With Us</h1>
-                        <p className='text-white md:text-[25px] text-[16px]'>Discover the breathtaking beauty, rich culture, and hidden gems of Pakistan like never before. From majestic mountains to vibrant cities, let us guide you on an unforgettable journey through this incredible land.</p>
+            <div className={styles.mainDiv}>
+                <div className={styles.innerDiv}>
+                    <div className={styles.parent}>
+                        <h1 className={styles.h1Div}>Explore Pakistan With Us</h1>
+                        <p className= {styles.pDiv}>Discover the breathtaking beauty, rich culture, and hidden gems of Pakistan like never before. From majestic mountains to vibrant cities, let us guide you on an unforgettable journey through this incredible land.</p>
                     </div>
                     <div>
-                        <Button className='text-left bg-white text-black hover:bg-[#4ee247]'><span><MdTravelExplore/></span>Travel Plan</Button>
+                        <Button className={styles.btn}><span><MdTravelExplore/></span>Travel Plan</Button>
                     </div>
                     <div>
-                        <h1 className='text-left text-white md:text-[45px] text-[25px]'>Explore the key points</h1>
+                        <h1 className={styles.h1Div2}>Explore the key points</h1>
                     </div>
-                    <div className='md:flex justify-start items-center gap-4 flex-row hidden'>
-                        <Button className='bg-white text-black hover:bg-[#4ee247]'><span><MdPlace/></span>Places</Button>
-                        <Button className='bg-white text-black hover:bg-[#4ee247]'><span><BiFoodMenu/></span> Restaurants</Button>
-                        <Button className='bg-white text-black hover:bg-[#4ee247]'><span><Hotel/></span>Hotels</Button>
-                        <Button className='bg-white text-black hover:bg-[#4ee247]'><span><ShoppingBagIcon/></span> Shopping</Button>
+                    <div className={styles.btnDiv}>
+                        <Button className={styles.btn2}><span><MdPlace/></span>Places</Button>
+                        <Button className={styles.btn2}><span><BiFoodMenu/></span> Restaurants</Button>
+                        <Button className={styles.btn2}><span><Hotel/></span>Hotels</Button>
+                        <Button className={styles.btn2}><span><ShoppingBagIcon/></span> Shopping</Button>
                     </div>
                 </div>
                 
