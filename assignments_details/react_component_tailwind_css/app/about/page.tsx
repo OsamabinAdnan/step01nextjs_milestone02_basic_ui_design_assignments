a03acdfaab7fc9280118f23cfe6744481aaf9086
OsamabinAdnan <binadnanosama@gmail.com>
2024-12-17 00:01:55 +0500

    first commit

--- assignments_details/react_component_tailwind_css/app/about/page.tsx
+++ assignments_details/react_component_tailwind_css/app/about/page.tsx
@@ -57,7 +57,7 @@ export default function About() {
                 {/* Left text part of top section */}
                 <div className='basis-[60%] max-w-full w-[50%] flex flex-col gap-[40px]'>
                     <h1 className='text-[54px] font-semibold'>Our Story</h1>
-                    <p className='text-[18px] font-normal'>Launced in 2015, Exclusive is South Asia's premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. </p>
+                    <p className='text-[18px] font-normal'>Launced in 2015, Exclusive is South Asia&apos;s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. </p>
                     <p className='text-[18px] font-normal'>Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging  from consumer.</p>
                 </div>
                 {/* Right picture of top section */}
